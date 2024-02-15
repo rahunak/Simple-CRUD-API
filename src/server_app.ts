54ddd997f8d407ec4ae574c5d7b44bf040e8ef1e
rahunak <euhene@linnovate.net>
2024-02-16 00:52:18 +0300

    #14: refactor

--- src/server_app.ts
+++ src/server_app.ts
@@ -3,14 +3,14 @@ import * as dotenv from 'dotenv'
 import serverHandler from './services/server_handler'
 
 dotenv.config()
-export let port = parseInt(process.env.PORT ?? '4000', 10)
+let port = parseInt(process.env.PORT ?? '4000', 10)
 
-export const startServerApp = (port: number) => {
-  return http.createServer((request: IncomingMessage, response: ServerResponse) => {
-    serverHandler(request, response)
-  }).listen(port, () => {
+const startServerApp = (port: number) => {
+  return http.createServer(serverHandler).listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
   })
 }
 
 startServerApp(port = 4000)
+
+export { startServerApp, port }
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -3,7 +3,7 @@ import { type IUser } from '../interfaces/interfaces'
 
 const storage: IUser[] = [
   {
-    id: 1, DataBaseUserID: uuidv4(), username: 'Maksim Znak', age: 30, hobbies: ['BOXING', 'Myau Thai', 'Rugby']
+    id: 1, DataBaseUserID: 'ef223a0e-38ed-454d-8ea5-4eb6bbc54445', username: 'Maksim Znak', age: 30, hobbies: ['BOXING', 'Myau Thai', 'Rugby']
   }
 ]
 
